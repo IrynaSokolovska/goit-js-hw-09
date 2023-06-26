1ae5de9a45cfaa3d2c7c14dcec7fa877b27e1ece
Iryna Sokolovska <sokirina777@gmail.com>
2023-06-26 15:39:54 +0300

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const colorBody = document.querySelector("body");
 
+
+stopBtn.setAttribute("disabled", "")
+let timerId = null
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
