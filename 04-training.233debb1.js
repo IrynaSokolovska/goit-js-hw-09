7237d69f3ff69f138610607bd6519c8556104381
IrynaSokolovska <122970815+IrynaSokolovska@users.noreply.github.com>
2023-06-28 13:00:49 +0000

    Deploying to gh-pages from @ IrynaSokolovska/goit-js-hw-09@58260a8da468b6e3473b9475a3d64cf2a8e98ee4 🚀

--- 04-training.233debb1.js
+++
@@ -1,2 +0,0 @@
-new Promise(((e,o)=>{const s=Math.random()>.5;setTimeout((()=>{s&&e("done sucsess успешно"),o("отклонено reject promis")}),2e3)})).then((e=>{console.log(e)}),(e=>{console.log(e)}));
-//# sourceMappingURL=04-training.233debb1.js.map
--- 04-training.4f8bb8ca.js
+++
@@ -1,2 +0,0 @@
-new Promise((function(o,n){var e=Math.random()>.5;setTimeout((function(){e&&o("done sucsess успешно"),n("отклонено reject promis")}),2e3)})).then((function(o){console.log(o)}),(function(o){console.log(o)}));
-//# sourceMappingURL=04-training.4f8bb8ca.js.map
