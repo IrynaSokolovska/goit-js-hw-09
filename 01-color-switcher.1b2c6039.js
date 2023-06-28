8a98cd8e5ebde79623f432af9edce276bd993cff
IrynaSokolovska <122970815+IrynaSokolovska@users.noreply.github.com>
2023-06-28 08:49:15 +0000

    Deploying to gh-pages from @ IrynaSokolovska/goit-js-hw-09@3288142cb003657566e2e9ea078c9e0dbd1de580 🚀

--- 01-color-switcher.1b2c6039.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("[data-start]");var t=document.querySelector("[data-stop]");document.querySelector("body");t.setAttribute("disabled","")}();
-//# sourceMappingURL=01-color-switcher.1b2c6039.js.map
--- 01-color-switcher.96496403.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[data-start]");const t=document.querySelector("[data-stop]");document.querySelector("body");t.setAttribute("disabled","");
-//# sourceMappingURL=01-color-switcher.96496403.js.map
